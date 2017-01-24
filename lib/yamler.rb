4ae9a9c3cafff6d07437ea91b5abfd717f525b10
Ravi Kumar <ravi.kumar@toptranslation.com>
2017-01-24 09:06:02 +0100

    support for array_index

--- lib/yamler.rb
+++ lib/yamler.rb
@@ -8,10 +8,8 @@ class Yamler
   def add_string(key, value, options={})
     seperator = options[:seperator] || '.'
     keys = key.split(seperator)
-
     keys.unshift(options[:locale_code]) if options[:locale_code]
-
-    @hash = add_to_hash(keys, value)
+    @hash = add_to_hash(keys, value, options[:array_index])
   end
 
   def to_hash
@@ -31,25 +29,30 @@ class Yamler
   end
 
   private
-    def add_to_hash(keys, value, hash=nil)
-      hash ||= @hash
-      result = hash.clone
-      first_key = keys.first
-      keys.shift
-
-      if result.has_key?(first_key)
-        result[first_key] = add_to_hash(keys, value, result[first_key])
-      else
-        if keys.empty?
-          result[first_key] = value
+  def add_to_hash(keys, value, array_index=nil, hash=nil)
+    hash ||= @hash
+    result = hash.clone
+    first_key = keys.first
+    keys.shift
+    if result.has_key?(first_key) && result[first_key].instance_of?(Hash)
+      result[first_key] = add_to_hash(keys, value, array_index, result[first_key])
+    else
+      if keys.empty?
+        if array_index
+          if result.keys.first == first_key
+             result[first_key][array_index] = value
+          else
+            result[first_key] = [].insert(array_index, value)
+          end
         else
-          new_hash = Hash.new
-          new_hash[first_key] = Hash.new
-
-          result[first_key] = add_to_hash(keys, value, new_hash[first_key])
+          result[first_key] = value
         end
+      else
+        new_hash = Hash.new
+        new_hash[first_key] = Hash.new
+        result[first_key] = add_to_hash(keys, value, array_index, new_hash[first_key])
       end
-
-      result
     end
+    result
+  end
 end
